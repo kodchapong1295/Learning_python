9c63990725305fc73a16ce702e862aacc4d45eaa
best <sunsasabest@hotmail.com>
2021-06-10 15:15:48 +0700

    [FINISH] snake game project

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,9 @@ screen.title("Snake Game")
 screen.tracer(0) #use to not update screen all the time
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -20,5 +25,23 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #Detech collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        scoreboard.write_score()
+
+    #Detech collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detech collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
