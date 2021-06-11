900e0a97f8e3a9d9d930e14e3c66437c7e0ed0c9
best <sunsasabest@hotmail.com>
2021-06-11 14:28:38 +0700

    [ADD] some method to improve snake game

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -6,6 +6,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -18,8 +19,15 @@ class ScoreBoard(Turtle):
         self.write_score()
 
     def write_score(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.write_score()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
