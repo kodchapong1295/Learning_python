866e506e3f607d8dcd2db39ea53549253f8894ee
best <sunsasabest@hotmail.com>
2021-06-11 21:18:39 +0700

    [EDIT] use list in list comprehension instead of create list with for loop

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -16,13 +16,15 @@ while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 Guess the State", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in correct_guesses:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in states_list:
+        #     if state not in correct_guesses:
+        #         missing_states.append(state)
+        missing_states = [state for state in states_list if state not in correct_guesses]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in states_list:
         tim = turtle.Turtle()
         tim.penup()
