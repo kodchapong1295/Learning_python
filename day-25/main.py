400177cce24f02f4bb67784d143a696912a8f588
best <sunsasabest@hotmail.com>
2021-06-11 15:58:24 +0700

    [FINISH] working with csv with pandas

--- day-25/main.py
+++ day-25/main.py
@@ -14,5 +14,39 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# average = sum(temp_list) / len(temp_list)
+average = data["temp"].mean()
+max_temp = data["temp"].max()
+print(max_temp)
+
+#Get Data in Columns
+print(data["condition"])
+print(data.condition)
+
+#Get Data in Rows
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
+
+#create DataFrame from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores":[76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+
 
