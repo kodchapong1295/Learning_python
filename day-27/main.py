7458541c7d72d368f603f7b04bbc52507400a2bd
best <sunsasabest@hotmail.com>
2021-06-12 14:42:51 +0700

    [ADD] playground.py

--- day-27/main.py
+++ day-27/main.py
@@ -2,6 +2,11 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+#Lable
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack() #auto center on screen
 
 
 
