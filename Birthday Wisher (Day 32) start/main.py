254c1dfdcd567b6f1cd17c13a61cfce7e75b6dcd
best <sunsasabest@hotmail.com>
2021-06-13 15:50:08 +0700

    [DONE] sending monday motivation

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,13 +1,51 @@
+# import smtplib
+#
+# my_email = "best.testsmtp@gmail.com"
+# password = "Best*1234"
+#
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="best.testsmtp@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
+
+# import datetime as dt
+#
+# #get data from datetime
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# #make datetime from scrach
+# date_of_birth = dt.datetime(year= 2001, month= 6, day= 1)
+# print(date_of_birth)
+
+#------------ Challenge 1: Monday Motivation --------------------
+import random
 import smtplib
+import datetime as dt
 
-my_email = "best.testsmtp@gmail.com"
-password = "Best*1234"
+MY_EMAIL = "best.testsmtp@gmail.com"
+MY_PASSWORD = "Best*1234"
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="best.testsmtp@yahoo.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+now = dt.datetime.now()
+weekday = now.weekday()
+if(weekday==0):
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
