ebc99eb4be75c77b4729c6b693200de33554d08c
best <sunsasabest@hotmail.com>
2021-06-26 15:58:26 +0700

    [ADD] add decorator function

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,6 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
+#------------------------------ Decorator Function ------------------------------#
+def make_bold(function):
+   def wrapper_function():
+       return "<b>"+function()+"</b>"
+   return wrapper_function
+
+def make_emphasis(function):
+   def wrapper_function():
+       return "<em>"+function()+"</em>"
+   return wrapper_function
+
+def make_underlined(function):
+   def wrapper_function():
+       return "<u>"+function()+"</u>"
+   return wrapper_function
+
+
 
 @app.route('/')
 def hello_world():
@@ -9,12 +26,17 @@ def hello_world():
            '<img src="https://media2.giphy.com/media/K1tgb1IUeBOgw/giphy.gif?cid=ecf05e47jy02icf5ac8qtvbli96j666j8vg3b4hjxtd58e39&rid=giphy.gif&ct=g">'
 
 @app.route('/bye')
+@make_bold
+@make_underlined
+@make_emphasis
 def say_bye():
     return 'Bye'
 
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
     return f"Hello {name}, you are {number} years old"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
