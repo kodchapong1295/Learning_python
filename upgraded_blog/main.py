dc104e207c6ccede12187b4005c65382f8adc7ee
best <sunsasabest@hotmail.com>
2021-07-10 21:08:39 +0700

    [FINISH] day60 challenge: successfully sent mail from blog website

--- upgraded_blog/main.py
+++ upgraded_blog/main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+from decouple import config
+
+MY_EMAIL = config('MY_EMAIL')
+PASSWORD = config('PASSWORD')
 
 posts = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
 
@@ -14,9 +19,32 @@ def get_all_posts():
 def show_about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST','GET'])
 def show_contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['msg']
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:New message from your blog website!\n\n"
+                    f"Name: {name}\n"
+                    f"Email: {email}\n"
+                    f"Phone: {phone}\n"
+                    f"Message: {message}"
+            )
+            print("Successfully sent an email!")
+
+        return render_template("contact.html", msg_sent=True)
+    else:
+        return render_template("contact.html", msg_sent=False)
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -25,6 +53,10 @@ def show_post(index):
         if blog_post['id'] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
