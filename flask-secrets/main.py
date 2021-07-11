bf20ad1e4a83860a0eec3da60f3e36cb3a2fea44
best <sunsasabest@hotmail.com>
2021-07-11 14:45:12 +0700

    [ADD] create form using WTForms

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label='Log In')
 
 app = Flask(__name__)
-
+app.secret_key = "hello secret world!"
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
